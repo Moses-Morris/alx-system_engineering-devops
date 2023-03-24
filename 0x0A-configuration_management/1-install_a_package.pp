16b2467776402470df57196dd2a4579cb72f3fec
Moses-Morris <mosesmorrisdev@gmail.com>
2023-03-24 11:02:53 -0700

    Install a package by gem provider.

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 #create a puppet instruction set to install a package
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem',
+package { 'flask':
+  ensure   => installed,
+  version => '2.1.0',
 }
