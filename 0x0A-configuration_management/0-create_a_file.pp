59997ff5b17f46048cb86504ba26470444e271e9
Moses-Morris <mosesmorrisdev@gmail.com>
2023-03-24 10:54:31 -0700

    Create a file with manifests.

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 #create a file this folder structure /tmp/school
 
-file{ 'school':
-ensure => 'present',
-content => 'I love Puppet',
-group => 'www-data',
-mode => '0744',
-owner => 'www-data',
-path => '/tmp/school',
+file { 'school':
+	ensure => 'present',
+	content => 'I love Puppet',
+	group => 'www-data',
+	mode => '0744',
+	owner => 'www-data',
+	path => '/tmp/school',
 }
